f823e2fa9043616e00836ce0abe0c939e68a06a5
Sudhir Kumar <sudhir.kr23.1995@gmail.com>
2019-10-04 10:27:48 +0530

    included form validation using express-validator

--- BACKEND/reg-app/server.js
+++ BACKEND/reg-app/server.js
@@ -1,6 +1,7 @@
 var express=require('express');
 var bodyParser=require('body-parser');
 var app=express();
+const { check, validationResult } = require('express-validator');
 const dotenv=require("dotenv");
 dotenv.config();
 //  mongodb atlas
@@ -28,14 +29,56 @@ app.get('/', (req, res) => {
         
     });
 // create new user
+// app.use(bodyParser.urlencoded(extended:true));
 app.use(bodyParser.json());
 
-app.post('/newUser',function(req,res){
-    db.collection('users').insertOne(req.body, function(error,result){
+app.post('/newUser',[ 
+   
+    check('full_name','Name cannot be left blank enter minimum 5 characters')
+        .isLength({ min: 5 }),
+   
+    check('email')
+       .isEmail().withMessage('Please enter a valid email address')
+       .trim()
+       .normalizeEmail(),
+       
+    check('password')
+       .isLength({ min: 6 }).withMessage('Password must be at least 6 chars long')
+       .matches(/\d/).withMessage('Password must contain one number'),
+       
+  check('gender','Please select gender')
+       .isLength({ min: 1 }),
+    check('dob','Date of birth cannot be left blank')
+       .isLength({ min: 1 }),
+    
+    check('country','Country cannot be left blank')
+       .isLength({ min: 1 })
+    
+   ]
+
+,function(req,res,next){
+    const errors=validationResult(req);
+    if(!errors.isEmpty()){
+        res.json({status:"error",message:errors.array()});
+    }
+    else{
+        var details={
+            full_name:   req.body.full_name,
+            email:       req.body.email,
+            password:    req.body.password, 
+            dob:         req.body.dob, 
+            country:     req.body.country, 
+            gender:      req.body.gender, 
+
+        };
+    db.collection('users').insertOne(details, function(error,result){
         if(error)
         throw error;
         res.json(result);
+        console.log({message:"user registered successfully"});
     });
+
+    }
  });
 //  update user details using id
 app.put('/updateUser', (req, res) => {
